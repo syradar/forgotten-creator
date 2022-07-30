21ded665f0b8ce875fb98132d181e008f0b5ce81
David Malmström <syntex@outlook.com>
2022-07-30 13:27:42 +0200

    add path resolve for locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,11 @@
+const path = import('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'sv'],
+    ...(typeof window === undefined
+      ? { localePath: path.resolve('./public/locales') }
+      : {}),
   },
 }
