f9176bec4eaee44f389c57770b8adb9450c494e3
David Malmström <syntex@outlook.com>
2022-07-30 14:13:16 +0200

    more configs

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const path = require('path')
+const path = require('path')
 
 // const isVercel =
 //   typeof window === 'undefined' && process.env.NODE_ENV === 'production'
@@ -10,6 +10,8 @@ module.exports = {
     locales: ['en', 'sv'],
     defaultNS: 'common',
     serializeConfig: false,
+    localePath: path.resolve('./public/locales'),
+
     // localePath: path.resolve(
     //   isVercel ? './public/static/locales' : './public/locales',
     // ),
